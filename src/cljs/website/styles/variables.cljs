9c095ef4fa6b13fb34d54a7b61562e5962269a44
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-11-02 15:34:45 +0100

    Add style variables

---
+++ src/cljs/website/styles/variables.cljs
@@ -0,0 +1,15 @@
+(ns website.variables)
+
+;; --- - --- - --- PREFIXES --- - --- - ---
+
+(def vendors
+  {:vendors ["moz" "webkit" "o"]})
+
+;; --- - --- - --- COLORS --- - --- - ---
+
+(def gray-1 "#f0f0f0")
+(def gray-2 "#ffffff")
+
+;; --- - --- - --- FONTS --- - --- - ---
+
+(def main-font "'Roboto'")
