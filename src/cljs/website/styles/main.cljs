bf7dc476eaba8863c6406ae44e731bfb002ef18a
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-11-07 21:17:06 +0100

    Add basic 'about me' article

--- src/cljs/website/styles/main.cljs
+++ src/cljs/website/styles/main.cljs
@@ -1,7 +1,16 @@
 (ns website.main-styles
   (:require [garden.core :refer [css]]
-            [garden.selectors :refer [defclass]]
+            [garden.selectors :refer [defclass p]]
+            [garden.stylesheet :refer [at-media]]
             [website.variables :refer [gray-1 white-1 main-font]]))
+
+(defclass container)
+(defclass fluid-container)
+
+(defclass article)
+(defclass article__separator)
+
+(defclass view)
 
 (def main
   (css [:html {:background-color gray-1
@@ -11,10 +20,28 @@
        [:body {:background-color white-1
                :min-height "100%"}]
 
-       [(defclass view) {:margin "0 auto"
-                         :padding-top "50px"}]
+       [:strong {:font-weight "bold"}]
 
-       [(defclass container) {:max-width "960px"
-                              :margin "0 auto"}]
+       [article__separator {:display "block"
+                            :text-align "center"
+                            :margin-bottom "10px"
+                            :font-size "20px"}]
 
-       [(defclass fluid-container) {:width "100%"}]))
+       [view {:margin "0 auto"
+              :padding-top "50px"}]
+
+       [article {:padding "10px 10px 20px 10px"
+                 :text-align "justify"}]
+
+
+       (at-media {:min-width "961px"}
+                 [container {:max-width "960px"
+                             :margin "0 auto"}]
+                 [article {:padding "0 10px 20px 0"}])
+
+       [fluid-container {:width "100%"}]
+
+       [:p {:margin-bottom "10px"
+            :font-size "19px"
+            :font-weight "lighter"
+            :line-height "25px"}]))
