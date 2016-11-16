1827cece40467f6ee0633722424fee2c9ecaf9ad
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-11-16 23:15:52 +0100

    Add FontAwesome with CDN

--- src/cljs/website/core.cljs
+++ src/cljs/website/core.cljs
@@ -22,7 +22,8 @@
 
 (defn current-page []
   [:div
-   [:link {:href "https://fonts.googleapis.com/css?family=Roboto:300,400" :rel "stylesheet"}]
+   [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"}]
+   [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Roboto:300,400"}]
    (style)
    [navbar state-name]
    [(session/get :current-page)]])
