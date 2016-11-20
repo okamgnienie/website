b40438c94b16405571fb799988e4bc1dfa87db96
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-11-20 17:13:39 +0100

    Include all navbar animations in the style element

--- src/cljs/website/styles/style.cljs
+++ src/cljs/website/styles/style.cljs
@@ -1,9 +1,9 @@
 (ns website.styles
-  (:require [website.navbar-animations :refer [home work]]
+  (:require [website.navbar-animations :refer [all]]
             [website.reset-styles :refer [reset]]
             [website.navbar-styles :refer [styles]]
             [website.main-styles :refer [main]]
             [website.footer-styles :as footer]))
 
 (defn style []
-  [:style reset main styles home work footer/styles])
+  [:style reset main styles all footer/styles])
