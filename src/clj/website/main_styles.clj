ddf94c1859d726da76e48aebe2ea7d1431a922c3
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-12-13 17:04:28 +0100

    Clean .noselect CSS class implementation

--- src/clj/website/main_styles.clj
+++ src/clj/website/main_styles.clj
@@ -18,12 +18,7 @@
 
 (defstyles styles
 
-  [noselect {:-webkit-touch-callout "none"
-             :-webkit-user-select "none"
-             :-khtml-user-select "none"
-             :-moz-user-select "none"
-             :-ms-user-select "none"
-             :user-select "none"}]
+  [noselect {:user-select "none"}]
 
   [:html :body app (attr :data-reactroot) {:height "100%"}]
 
