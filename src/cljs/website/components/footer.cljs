9e896a14ac4bd312375cf75396fe710dfcaca00b
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-12-15 00:20:46 +0100

    Open contact links in new tab

--- src/cljs/website/components/footer.cljs
+++ src/cljs/website/components/footer.cljs
@@ -5,17 +5,27 @@
    [:div {:class "container"}
     [:div {:class "footer__credits"} "Przemyslaw Hardyn • 2016 • Geneva"]
     [:div {:class "footer__contact-icons"}
-     [:a {:class "footer__link footer__link--linkedin" :href "https://linkedin.com/in/phardyn"}
+     [:a {:target "_blank"
+          :class "footer__link footer__link--linkedin"
+          :href "https://linkedin.com/in/phardyn"}
       [:i {:class "fa fa-linkedin fa-1x"}]]
 
-     [:a {:class "footer__link footer__link--facebook" :href "https://facebook.com/przemyslawhardyn"}
+     [:a {:target "_blank"
+          :class "footer__link footer__link--facebook"
+          :href "https://facebook.com/przemyslawhardyn"}
       [:i {:class "fa fa-facebook fa-1x"}]]
 
-     [:a {:class "footer__link footer__link--github" :href "https://github.com/phardyn"}
+     [:a {:target "_blank"
+          :class "footer__link footer__link--github"
+          :href "https://github.com/phardyn"}
       [:i {:class "fa fa-github fa-1x"}]]
 
-     [:a {:class "footer__link footer__link--twitter" :href "https://t.com/p_hardyn"}
+     [:a {:target "_blank"
+          :class "footer__link footer__link--twitter"
+          :href "https://t.com/p_hardyn"}
       [:i {:class "fa fa-twitter fa-1x"}]]
 
-     [:a {:class "footer__link footer__link--vsco" :href "https://vsco.co/hardyn"}
+     [:a {:target "_blank"
+          :class "footer__link footer__link--vsco"
+          :href "https://vsco.co/hardyn"}
       [:i {:class "footer__vsco-icon"} "VSCO"]]]]])
--- src/cljs/website/pages/contact.cljs
+++ src/cljs/website/pages/contact.cljs
@@ -5,19 +5,29 @@
    [:div {:class "container"}
     [:div {:class "contacts"}
 
-     [:a {:class "contacts__icon" :href "https://linkedin.com/in/phardyn"}
+     [:a {:target "_blank"
+          :class "contacts__icon"
+          :href "https://linkedin.com/in/phardyn"}
       [:i {:class "fa fa-linkedin fa-3x"}]]
 
-     [:a {:class "contacts__icon" :href "https://facebook.com/przemyslawhardyn"}
+     [:a {:target "_blank"
+          :class "contacts__icon"
+          :href "https://facebook.com/przemyslawhardyn"}
       [:i {:class "fa fa-facebook fa-3x"}]]
 
-     [:a {:class "contacts__icon" :href "https://github.com/phardyn"}
+     [:a {:target "_blank"
+          :class "contacts__icon"
+          :href "https://github.com/phardyn"}
       [:i {:class "fa fa-github fa-3x"}]]
 
-     [:a {:class "contacts__icon" :href "https://t.com/p_hardyn"}
+     [:a {:target "_blank"
+          :class "contacts__icon"
+          :href "https://t.com/p_hardyn"}
       [:i {:class "fa fa-twitter fa-3x"}]]
 
-     [:a {:class "contacts__icon contacts__icon--vsco" :href "https://vsco.co/hardyn"}
+     [:a {:target "_blank"
+          :class "contacts__icon contacts__icon--vsco"
+          :href "https://vsco.co/hardyn"}
       [:i "VSCO"]]]
 
     [:form {:class "contact-form"}
