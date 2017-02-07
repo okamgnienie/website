7ced8ae862c4deffe72b593e8f969d3e93308f52
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-02-08 00:21:49 +0100

    Use hiccup dot notation and map instead of for loop in hours view

--- src/cljs/website/pages/hours.cljs
+++ src/cljs/website/pages/hours.cljs
@@ -1,11 +1,11 @@
 (ns website.hours)
 
 (defn hours []
-  [:div {:class "view view--hours"}
-   [:div {:class "container"}
-    [:div {:class "hours-gallery"}
+  [:div.view.view--hours
+   [:div.container
+    [:div.hours-gallery
 
-     (for [item (range 1 100)]
-       [:img {:key item
-              :class "hours-gallery__item"
-              :src (str "/images/hours/" item ".jpg")}])]]])
+     (map (fn [k]
+            [:img.hours-gallery__item {:key k
+                                       :src (str "/images/hours/" k ".jpg")}])
+          (range 1 100))]]])
