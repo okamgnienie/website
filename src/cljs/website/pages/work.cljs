73fcbd7527959b4a806d418b1209c65d2a42bb66
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-02-08 00:14:23 +0100

    Switch to hiccup dot notation for css classes in work view

--- src/cljs/website/pages/work.cljs
+++ src/cljs/website/pages/work.cljs
@@ -1,5 +1,5 @@
 (ns website.work)
 
 (defn work []
-  [:div {:class "view view--work"}
-   [:div {:class "container"} "All the hackathons and work related info"]])
+  [:div.view.view--work
+   [:div.container "All the hackathons and work related info"]])
