68e39c45bde4a4ad0b93fb3b7b24856fec084fe0
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-04-06 16:51:06 +0200

    Add 'work in progress' message on the work page

--- src/cljs/website/pages/work.cljs
+++ src/cljs/website/pages/work.cljs
@@ -1,5 +1,19 @@
-(ns website.work)
+(ns website.work
+  (:require [website.message :refer [message]]))
+
+(defn work-in-progress-message []
+  [message
+   "Creation of this section is currently in progress"
+   [:span
+    "In the near future you'll be able  to view my professional experience, "
+    "projects and hackathons I took part under this url."
+    [:br]
+    "For now please visit my LinkedIn profile "
+    [:a {:href "https://linkedin.com/in/phardyn" :target "_blank"} "here"]
+    "."]
+   "bright"])
 
 (defn work []
   [:div.view.view--work
-   [:div.container "All the hackathons and work related info"]])
+   [:div.container
+    [work-in-progress-message]]])
