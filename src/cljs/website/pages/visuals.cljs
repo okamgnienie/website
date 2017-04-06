1a8a182385cc7d7dace936308f6e7f36abe5e197
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-04-06 18:10:00 +0200

    Replace current visuals page content with 'in progress' message

--- src/cljs/website/pages/visuals.cljs
+++ src/cljs/website/pages/visuals.cljs
@@ -1,104 +1,16 @@
 (ns website.visuals
-  (:require [reagent.core :as r]
-            [clojure.string :refer [blank?]]
-            [website.visuals-service :refer [get-visuals]]))
+  (:require [website.message :refer [message]]))
 
-;; ----------
-;; View model
+;; ------------------------
+;; Work in progress message
 
-(def filter-query (r/atom ""))
-
-(def visuals-data (r/atom []))
-
-(def filtered-visuals-data (r/atom []))
-
-(def page (r/atom 1))
-
-(def max-page (r/atom 1))
-
-(get-visuals visuals-data)
-
-;; -----------------------
-;; Component functionality
-
-(defn filter-visuals [query]
-  (reset! filter-query query)
-
-  (reset! page 1)
-
-  (reset! filtered-visuals-data
-          (vec (doall (filter
-                       (fn [x]
-                         (some #{@filter-query} (x :tags))) @visuals-data)))))
-
-(defn limit-results [page-number results]
-  (js/scroll 0 0)
-
-  (reset! max-page
-          (int (Math/ceil
-                (/ (count results) 5))))
-
-  (if (> (count results) 5)
-    (take 5 (subvec results (* (dec page-number) 5)))
-    results))
-
-
-(defn reset-filter []
-  (reset! page 1)
-  (reset! filter-query ""))
-
-;; -------------------------
-;; Component to build images
-
-(defn image [data]
-  [:a {:class "visual__link" :href (data :image)
-       :style {:background-color (data :background)}}
-   [:img {:class "visual__img":src (data :image)}]])
-
-;; ----------------------------------
-;; Component to build embedded videos
-
-(defn embedded-video [code]
-  [:div {:class "visual__embedded-video"
-         :dangerouslySetInnerHTML {:__html code}}])
-
-;; -----------------------
-;; Single visual component
-
-(defn visual [data]
-  [:div {:class (str "visual visual--" (data :type))}
-
-   (when (= (data :type) "image")
-     [image data])
-
-   (when (= (data :type) "embedded-video")
-     [embedded-video (data :code)])
-
-   [:div {:class "visual__title"} (data :title)
-    (doall (for [tag (data :tags)]
-             ^{:key tag} [:span {:class (str
-                                         "visual__tag noselect "
-                                         (when (= tag @filter-query) "visual__tag--active"))
-                                 :on-click  #(filter-visuals tag)} tag]))]])
-
-;; -------------------------
-;; Pagination component
-
-(defn manage-page [page-number]
-  [:div {:class "visuals-pagination noselect"}
-   [:a {:class (str "visuals-pagination__arrow visuals-pagination__arrow--left "
-                    (when (= page-number 1)
-                      "visuals-pagination__arrow--disabled"))
-        :href (str "#/visuals/" (dec page-number))}
-    [:i {:class "fa fa-long-arrow-left" :aria-hidden "true"}]]
-
-   [:span {:class "visuals-pagination__current"} page-number]
-
-   [:a {:class (str "visuals-pagination__arrow visuals-pagination__arrow--right "
-                    (when (= page-number @max-page)
-                      "visuals-pagination__arrow--disabled"))
-        :href (str "#/visuals/" (inc page-number))}
-    [:i {:class "fa fa-long-arrow-right" :aria-hidden "true"}]]])
+(defn work-in-progress-message []
+  [message
+   "Creation of this section is currently in progress"
+   [:span
+    "Come back in the near future to see the " [:strong "Visuals"] " page. "
+    "It will contain my personal work and all sort of cool things I managed to collect over the Internet."]
+   "dark"])
 
 ;; ----------------------
 ;; Visuals page main view
@@ -106,22 +18,4 @@
 (defn visuals [page-number]
   [:div {:class "view view--visuals"}
    [:div {:class "container"}
-    [:div {:class "options"}
-     [:div {:class "filter"}
-      [:input {:type "text"
-               :placeholder "filter"
-               :class "filter__input"
-               :value @filter-query
-               :on-change #(filter-visuals (-> % .-target .-value))}]
-      [:button {:type "button"
-                :class (str "filter__clear-btn" (when (blank? @filter-query) " filter__clear-btn--visible"))
-                :on-click reset-filter} "clear"]
-      [:div {:class "filter__progress"}]]]
-
-    [:div {:class "visuals-gallery"}
-     (for [item (limit-results page-number (if (blank? @filter-query)
-                                             @visuals-data
-                                             @filtered-visuals-data))]
-       ^{:key (item :id)} [visual item])]
-
-    [manage-page page-number]]])
+    [work-in-progress-message]]])
